f6c4386b5f62da5995ec8b42b9d689c9febb5d7e
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-03-29 01:18:07 -0300

    add zod , clear proyect

---
+++ frontend/shemas/shemas.ts
@@ -0,0 +1,19 @@
+import {z} from "zod"
+
+// Register 
+
+export const RegisterShema = z.object({
+    email:z
+    .string()
+    .email({ message: "Ingrese un correo electrónico válido" }),
+    password: z
+    .string()
+    .min(6, { message: "La contraseña debe tener al menos 6 caracteres" }),
+    confirmPassword: z
+    .string()
+    .min(6, { message: "Se recomienda que la contraseña tenga al menos 6 caracteres"}),
+})
+.refine((data)=> data.password === data.confirmPassword, {
+    message: "Password no coinciden",
+    path:["confirmPassword"],
+})
