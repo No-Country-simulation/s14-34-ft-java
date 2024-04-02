ba5b967e44a6f4804da26549ef8d28b52edbb1fe
Matias Giorgini <msgiorgini77@gmail.com>
2024-04-02 01:42:18 -0300

    Entidades

---
+++ backend/NoCountry/src/main/java/model/entity/Pet.java
@@ -0,0 +1,15 @@
+package model.entity;
+
+public class Pet {
+    private String name;
+    private String breed;
+    private String colour;
+    private Integer age;
+    private String size;
+    private String behaviour;
+    private String healthStatus;
+    private String location;
+    private Boolean vaccinated;
+    private Boolean sterilized;
+    private String generalDescription;
+}
---
+++ backend/NoCountry/src/main/java/model/entity/User.java
@@ -0,0 +1,10 @@
+package model.entity;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private int phone;
+    private int dni;
+}
