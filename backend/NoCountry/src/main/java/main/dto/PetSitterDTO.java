4826afa7919f6dc72af93d572e6c52a3ccf4d986
PatEche <echeverriaple@gmail.com>
2024-04-19 16:28:22 -0300

    [Feature] Entity PetSitter

---
+++ backend/NoCountry/src/main/java/main/dto/PetSitterDTO.java
@@ -0,0 +1,12 @@
+package main.dto;
+
+import lombok.Data;
+
+@Data
+public class PetSitterDTO {
+    private long id;
+    private String firstName;
+    private String lastName;
+
+
+}
---
+++ backend/NoCountry/src/main/java/main/models/PetSitter.java
@@ -0,0 +1,48 @@
+package main.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "PET_SITTERS")
+public class PetSitter {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPetSitter;
+
+    @Column(name = "fist_name", length = 50, nullable = false)
+    private String firstName;
+    @Column(name = "last_name", length = 50, nullable = false)
+    private String lastName;
+    @Column(name = "phone", length = 50, nullable = false, unique = true)
+    private String phone;
+    @Column(name = "address", length = 50, nullable = false, unique = true)
+    private String address;
+    @Column(name = "location", length = 50, nullable = false)
+    private String location;
+    @Column(name = "province", length = 50, nullable = false)
+    private String province;
+    @Column(name = "postalcode", length = 50, nullable = false)
+    private String postalCode;
+    @Column(name = "dni", length = 20, nullable = false, unique = true)
+    private String dni;
+    @Column(name = "email", length = 50, nullable = false, unique = true)
+    private String email;
+    @Column(name = "photo_url", length = 20, nullable = false, unique = true)
+    private String photo;
+    @Column(name = "available")
+    private Boolean available = true;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_service")
+    private Service service;
+
+
+}
