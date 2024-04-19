30e5c8dd562862ca3e1a3de8c10c955c3bf12606
Nico <nlemos@rec.utn.edu.ar>
2024-04-19 14:19:37 -0300

    borrando imports

--- backend/NoCountry/src/main/java/main/models/Pet.java
+++ backend/NoCountry/src/main/java/main/models/Pet.java
@@ -1,7 +1,7 @@
 package main.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
