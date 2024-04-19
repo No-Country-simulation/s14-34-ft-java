d0829605c5e0987f742081c7569fc9db99dcf404
Valerie Ramos <valerie22@live.com>
2024-04-19 12:56:04 -0500

    booking updates

---
+++ backend/NoCountry/src/main/java/main/enums/TypeOfPet.java
@@ -0,0 +1,15 @@
+package main.enums;
+
+public enum TypeOfPet {
+
+    DOG,
+    CAT,
+    CHINCHILLA,
+    HAMSTER,
+    RABBIT,
+    FERRET,
+    HEDGEHOG,
+    GROUND,
+    GUINEA_PIG
+
+}
---
+++ backend/NoCountry/src/main/java/main/enums/TypeOfService.java
@@ -0,0 +1,9 @@
+package main.enums;
+
+public enum TypeOfService {
+
+    PET_WALKING,
+    HOUSE_SITTING,
+    HOME_VISITS
+
+}
