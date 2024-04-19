dfed42b18b9d152e276c3a73f3bcf6855247a6f2
Valerie Ramos <valerie22@live.com>
2024-04-19 14:23:07 -0500

    deleted enum Type

--- backend/NoCountry/src/main/java/main/dtos/PetDTO.java
+++ backend/NoCountry/src/main/java/main/dtos/PetDTO.java
@@ -1,12 +1,12 @@
 package main.dtos;
 
 import main.enums.PetSize;
-import main.enums.Type;
+import main.enums.TypeOfPet;
 
 public record PetDTO(
         Long id,
         String photo,
-        Type type,
+        TypeOfPet typeOfPet,
         String name,
         String breed,
         Integer age,
