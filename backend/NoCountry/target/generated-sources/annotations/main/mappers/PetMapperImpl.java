73eb3189f34b4571ac28ca63fe5a566226fc642c
Matias Giorgini <msgiorgini77@gmail.com>
2024-04-22 23:20:43 -0300

    Test 2

--- backend/NoCountry/target/generated-sources/annotations/main/mappers/PetMapperImpl.java
+++ backend/NoCountry/target/generated-sources/annotations/main/mappers/PetMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-04-22T22:51:08-0300",
+    date = "2024-04-22T23:19:37-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
 )
 @Component
