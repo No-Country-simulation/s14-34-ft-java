e412753f910633c38f80d9291dcd50212bb11add
Nico <nlemos@rec.utn.edu.ar>
2024-04-26 18:14:19 -0300

    cambie la query del repository a JPQL en vez de Nativa

--- backend/NoCountry/src/main/java/main/repository/OwnerRepository.java
+++ backend/NoCountry/src/main/java/main/repository/OwnerRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
-    // Puedes agregar métodos personalizados de consulta si es necesario
 
-    @Query(value = "select id from owners where user_id = ?1",nativeQuery = true)
-    Long findOwnerIdByUserId(Long userId);
+
+    @Query("SELECT o.id FROM Owner o WHERE o.user.user_id = :userId")
+    Long findOwnerIdByUserId(@Param("userId") Long userId);
 }
 
